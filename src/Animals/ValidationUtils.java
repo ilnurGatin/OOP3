6573ec9f3c1b6feed16046ec17a5ca6ee1fe44cf
ilnurGatin <reilag@yandex.ru>
2022-10-22 01:16:40 +0500

    Initial commit

---
+++ src/Animals/ValidationUtils.java
@@ -0,0 +1,13 @@
+package Animals;
+
+public class ValidationUtils {
+
+    public static String validOrDefault(String value, String defaultValue) {
+        if (value != null && !value.isEmpty() && !value.isBlank()) {
+            return value;
+        } else {
+            return defaultValue;
+        }
+    }
+
+}
